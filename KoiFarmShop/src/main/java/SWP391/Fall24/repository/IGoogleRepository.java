0beee627f5548555a93c6d1eca2b41e75706dada
quanjokn <quanctse182750@fpt.edu.vn>
2024-10-04 11:13:41 +0700

    delete google login temporarily

--- KoiFarmShop/src/main/java/SWP391/Fall24/repository/IGoogleRepository.java
+++ KoiFarmShop/src/main/java/SWP391/Fall24/repository/IGoogleRepository.java
@@ -1,12 +1,12 @@
-package SWP391.Fall24.repository;
-
-import SWP391.Fall24.pojo.Users;
-import org.apache.catalina.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-@Repository
-public interface IGoogleRepository extends JpaRepository<Users , Integer> {
-    Optional<Users> findByEmail(String email);
-}
+//package SWP391.Fall24.repository;
+//
+//import SWP391.Fall24.pojo.Users;
+//import org.apache.catalina.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//@Repository
+//public interface IGoogleRepository extends JpaRepository<Users , Integer> {
+//    Optional<Users> findByEmail(String email);
+//}
--- KoiFarmShop/src/main/java/SWP391/Fall24/security/GoogleSecurityConfig.java
+++ KoiFarmShop/src/main/java/SWP391/Fall24/security/GoogleSecurityConfig.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class GoogleSecurityConfig {
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChainGoogleConfig (HttpSecurity http) throws Exception{
-        http
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/save",true)
-                );
-        return http.build();
-    }
-}
+//
+//@Configuration
+//public class GoogleSecurityConfig {
+//
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChainGoogleConfig (HttpSecurity http) throws Exception{
+//        http
+//                .oauth2Login(oauth2 -> oauth2
+//                        .defaultSuccessUrl("/save",true)
+//                );
+//        return http.build();
+//    }
+//}
